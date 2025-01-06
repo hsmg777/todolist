9044b0bd0dddd53b24801707557f70a661d3fc51
osccarJR <oscaremilioalbornoz@hotmail.com>
2025-01-06 11:14:36 -0500

    Nuevo test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,26 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders TaskDo text on the main page', () => {
+  render(
+    <MemoryRouter initialEntries={['/']}>
+      <App />
+    </MemoryRouter>
+  );
+
+  const taskDoText = screen.getByText(/TaskDo/i);
+  expect(taskDoText).toBeInTheDocument();
+});
+
+test('renders the dashboard when navigating to /dashboard', () => {
+  render(
+    <MemoryRouter initialEntries={['/dashboard']}>
+      <App />
+    </MemoryRouter>
+  );
+
+  const dashboardText = screen.getByText(/TaskDo/i); // Ajusta este texto si necesitas algo diferente
+  expect(dashboardText).toBeInTheDocument();
 });
